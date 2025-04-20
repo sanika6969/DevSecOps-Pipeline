31bb6a79388668b767ae8d711f78e360e221e31c
Kaushik Aduri <85384806+Kok363@users.noreply.github.com>
2025-04-21 00:20:11 +0530

    Update BoardGame.java
    used lombok in BoardGame.java to replace the getter and setter methods

--- src/main/java/com/javaproject/beans/BoardGame.java
+++ src/main/java/com/javaproject/beans/BoardGame.java
@@ -1,5 +1,8 @@
 package com.javaproject.beans;
- 
+
+import lombok.Data;
+
+@Data
 public class BoardGame {
     private String name;
     private String level;
@@ -7,32 +10,4 @@ public class BoardGame {
     private int maxPlayers;
     private String gameType;
     private Long id;
- 
-    // Add the getter methods
- public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-   
- public String getName() {
-        return name;
-    }
- 
-    public String getLevel() {
-        return level;
-    }
- 
-    public int getMinPlayers() {
-        return minPlayers;
-    }
- 
-    public int getMaxPlayers() {
-        return maxPlayers;
-    }
- 
-    public String getGameType() {
-        return gameType;
-    }
 }
