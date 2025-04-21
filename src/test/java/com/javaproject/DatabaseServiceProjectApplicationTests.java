ba236e13b067ec91af0463f14027133b11463fbf
Kaushik Aduri <85384806+Kok363@users.noreply.github.com>
2025-04-21 12:40:53 +0530

    Update DatabaseServiceProjectApplicationTests.java
    Updated

--- src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
+++ src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
@@ -2,14 +2,12 @@ package com.javaproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = DatabaseServiceProjectApplication.class)
-@ActiveProfiles("test")
+@SpringBootTest
 class DatabaseServiceProjectApplicationTests {
 
     @Test
     void contextLoads() {
-        // Basic context loading test
     }
 }
+
