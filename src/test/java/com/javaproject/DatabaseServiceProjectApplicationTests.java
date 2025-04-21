f90f935f38025f3c1a217579db378518505bbf83
Kaushik Aduri <85384806+Kok363@users.noreply.github.com>
2025-04-21 12:33:07 +0530

    Update DatabaseServiceProjectApplicationTests.java
    Context uses as in-memory (H2) database

--- src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
+++ src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
@@ -1,14 +1,15 @@
 package com.javaproject;
 
 import org.junit.jupiter.api.Test;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(classes = DatabaseServiceProjectApplication.class)
+@ActiveProfiles("test")
 class DatabaseServiceProjectApplicationTests {
+
     @Test
     void contextLoads() {
+        // Basic context loading test
     }
 }
